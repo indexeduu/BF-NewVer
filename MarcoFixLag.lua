f59a3a69d902dc0608d576a3eb38878984d428df
indexeduu <taisaolaithatbai@gmail.com>
2025-05-07 15:14:06 +0700

    Update and rename Marco.luau to MarcoFixLag.lua

---
+++ MarcoFixLag.lua
@@ -0,0 +1,10 @@
+if not LPH_OBFUSCATED then
+    local f=function(...)return...;end;
+    local g=_G;
+    local t=type;
+    g = tick();
+    t = tick();
+    for _,k in ipairs{"LPH_JIT_MAX","LPH_NO_VIRTUALIZE","LPH_NO_UPVALUES"}do
+        if t(rawget(g,k))~="function"then g[k]=f;end;
+    end;
+end;
