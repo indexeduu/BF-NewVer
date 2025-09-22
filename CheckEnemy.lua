7828b3ea00308bea6b92c28ae2f0cc379cc81c25
indexeduu <taisaolaithatbai@gmail.com>
2025-09-22 17:03:38 +0700

    Create CheckEnemy.lua

---
+++ CheckEnemy.lua
@@ -0,0 +1,21 @@
+volcano_function = function(...) return ... end
+if not LPH_OBFUSCATED then
+	LPH_JIT_MAX = volcano_function;
+	LPH_NO_VIRTUALIZE = volcano_function;
+	LPH_NO_UPVALUES = volcano_function;
+end;
+Check_Monster = LPH_JIT_MAX(function(Monster)
+	local Table_Monster = Monster;
+	if type(Monster) == "string" then Table_Monster = {Monster} end;
+	local nearestEnemy = nil;
+	local shortestDistance = math.huge;
+
+	for _, v in pairs(workspace.Enemies:GetChildren()) do
+		if table.find(Table_Monster, v.Name) and Check_Alive_Monster(v) and Distance(v.HumanoidRootPart.Position) <= 2000 then
+			local distance = Distance(v.HumanoidRootPart.Position);
+			if distance < shortestDistance then
+				nearestEnemy = v;
+				shortestDistance = distance;
+			end;
+		end;
+	end;
